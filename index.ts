82e866b168392a99bcc898d6904f1fc279ccd73c
mentorvikash <mentorvikash@gmail.com>
2023-09-30 15:06:03 +0530

    copy one file data to other

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import {serve} from 'bun'
 
-const server = serve({
-    fetch(){
-        return new Response(" enpoint make change")
-    },
-    port: 4000
-}) 
+// READ AND WRITE Files
 
+// Bun.file pointed to data.txt | -- even if no file exist it target a black space.
+const input  = Bun.file("data.txt");
+const copyToFile = Bun.file("data_copy.txt");
+await Bun.write(copyToFile, input);
 
-console.log("serve is running at" + server.port)
+// Write text content to file
+const textdata = "text data to write to a file"
+// --If not file exists create new file. 
+const output = Bun.file('output.txt');
+Bun.write(output, textdata);
