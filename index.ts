f09e8ffca9644736d981305defe6ac870e2ef4a3
mentorvikash <mentorvikash@gmail.com>
2023-09-30 16:01:45 +0530

    add encoded data

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 
+await foo.text(); // contents as a string
+ await foo.stream(); // contents as ReadableStream
+await foo.arrayBuffer();
+
 // READ AND WRITE Files
 
 // Bun.file pointed to data.txt | -- even if no file exist it target a black space.
@@ -11,3 +15,12 @@ const textdata = "text data to write to a file"
 // --If not file exists create new file. 
 const output = Bun.file('output.txt');
 Bun.write(output, textdata);
+
+
+// TEXT ENCODING.
+
+const encoder = new TextEncoder()
+const text_to_encode = "try to incode me if possible"
+const data = encoder.encode(text_to_encode)
+await Bun.write("encoded_data.txt", data)
+
